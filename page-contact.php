0561ece1feccfbe4f2e37b89e054ef7c3da158f0
Jereme <jereme.daniels@gmail.com>
2019-06-20 20:16:13 -0400

    contact page

--- page-contact.php
+++ page-contact.php
@@ -1,7 +1,39 @@
 <?php 
     get_header();
 ?>
-contact page
+<div class="banner-div">
+    <!-- dynamic should be loaded from WP -->
+    <img src="<?php echo get_template_directory_uri()?>/img/contact-banner-1900.jpg" class="desktop-img"alt="">
+    <img src="<?php echo get_template_directory_uri()?>/img/contact-banner-900.jpg"class="mobile-img" alt="">
+</div>
+<div id="contact-social">
+    <a href="#" target="_BLANK">
+        <img src="<?php echo get_template_directory_uri()?>/img/facebook.svg" alt="">
+    </a>
+    <a href="#" target="_BLANK">
+        <img src="<?php echo get_template_directory_uri()?>/img/linkedin.svg" alt="">
+    </a>
+    <a href="#" target="_BLANK">
+        <img src="<?php echo get_template_directory_uri()?>/img/mail.svg" alt="">
+    </a>
+    <a href="#" target="_BLANK">
+        <img src="<?php echo get_template_directory_uri()?>/img/phone.svg" alt="">
+    </a>
+    <a href="#" target="_BLANK">
+        <img src="<?php echo get_template_directory_uri()?>/img/instagram.svg" alt="">
+    </a>
+</div>
+<div id=contact-section-divs> 
+<section class="contact-section">
+    <div id="contact-address">
+        <!-- <h2>Get in touch</h2> -->
+        <h3>Brushy Fork Environmental Consulting, Inc</h3>
+        <p>10565 Hwy 421</p>
+        <p>Trade, TN 37691</p>
+        <p><a href="#">info@bfec</a></p>
+        <p>Connect with us on any of the provided social networks or a quick message.</p>
+    </div>
+</section>
 <?php 
      while(have_posts()) {
         the_post();
@@ -14,6 +46,12 @@ contact page
      }
 ?>
             </section>
+            <section>
+                <div>
+
+                </div>
+            </section>
+    </div>
 <?php 
     get_footer();
 ?>
